536da9c43b0dc9f7ea1c06031412fdde98b36d14
dudu-mobila <eduardojojotody@gmail.com>
2025-04-26 11:18:24 -0300

    aula 0 competec

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
-	}
+	    Scanner entrada=new Scanner(System.in);
+		char eduardo='e';
+		int idade=entrada.nextInt();
+		boolean cafe=entrada.nextBoolean();
+		System.out.println("digite o valor da laranja");
+		int laranja=entrada.nextInt();
+		System.out.println("digite o valor da uva");
+		int uva=entrada.nextInt();
+		int soma=laranja+uva;
+		System.out.println("resultado"+soma);
+		
+		
+		}
 }
