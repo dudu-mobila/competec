82b746a2d222b8c462f4f0e9528441ea8dc00c87
dudu-mobila <eduardojojotody@gmail.com>
2025-05-10 11:59:45 -0300

    aula-01

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		char eduardo = 'e';
+		char caio = 'c';
+		System.out.println(eduardo > caio);
+		System.out.println(true);
+		int idadelucas = 19;
+		int idadeeduardo = 15;
+		System.out.println(idadelucas > idadeeduardo);
+		System.out.println(true);
+		boolean chovendo = true;
+		boolean relampagos = false;
+		if (chovendo||relampagos) {
+		    System.out.println("não irei sair de casa");
+		}
+		else {
+		    System.out.println("irei sair de casa");
+		}
+		boolean strogonoff = true;
+		boolean macarrao = true;
+		if (strogonoff || macarrao){
+		    System.out.println("ficarei muito feliz");
+		}
+		else {
+		    System.out.println("nao ficarei nada feliz");
+		}
 	}
-}
+}	
+	
+
